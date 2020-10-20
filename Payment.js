9f5c37c8b06b0044f2e39059e1e47f8d5c64c4ea
Vasily Smolyar <smolyar4@gmail.com>
2020-10-20 20:48:50 +0300

    Новый дизайн платежей

--- Payment.js
+++ Payment.js
@@ -6,7 +6,7 @@ import Constants from 'expo-constants';
 import ModalList from './components/ModalList';
 import ModalMap from './components/ModalMap';
 import send from './utils/net';
-import passport from './files/passport.png';
+import arrow from './files/blackArrow.png';
 import path from './files/path.png';
 
 export default function PaymentScreen({navigation}) {
@@ -26,6 +26,7 @@ export default function PaymentScreen({navigation}) {
     const [corpus, setCorpus] = useState("");
     const [stage, setStage] = useState("");
     const [doorphone, setDoorphone] = useState("");
+    const [comment, setComment] = useState("");
 
     const choiceRegion = (name) => {
         setRegion(name);
@@ -76,79 +77,91 @@ export default function PaymentScreen({navigation}) {
 
     return (
         <View styles={styles.container}>
-            <StatusBar style="light" />
+            <StatusBar style="dark" />
             <View style={styles.barContainer}>
-                <Text style={styles.barText}>Оформление заказа</Text>
+                <View style={styles.barCell}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Image source={arrow} style={{width: 50, height: 25}} resizeMode='contain' />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.barCell}>
+                    <Text style={styles.barText}>Оформление заказа</Text>
+                </View>
             </View>
             <ScrollView style={{backgroundColor: 'white'}}>
                 <ModalList visible={modal} onChoice={(item) => choiceRegion(item.area_name)} onExit={() => setModal(false)} />
                 <ModalMap visible={map} close={() => setMap(false)} locate={choiceLocate} />
-                <View style={styles.warning}>
-                    <Image source={passport} style={styles.warningImage} resizeMode={'contain'} />
-                    <View>
-                        <Text style={styles.warningText}>Приготовьте паспорт</Text>
-                        <Text style={styles.warningFull}>Курьер вправе потребовать документ, удостоверяющий личность получателя в соответствии с п. 2 ст. 16 171-ФЗ Российской федерации</Text>
-                    </View>
-                </View>
-                <Text style={styles.header}>Данные получателя</Text>
-                <View style={styles.inputWrap}>
-                    <Text style={styles.inputWrapText}>Имя и фамилия</Text>
-                    <TextInput value={login} style={styles.phone} onChangeText={(text) => setLogin(text)} />
-                </View>
-                <View style={[styles.inputWrap, {width: '50%'}]}>
-                    <Text style={styles.inputWrapText}>Номер телефона</Text>
-                    <TextInput value={'+7' + phone} onChangeText={(text) => setPhone(text.slice(2))} maxLength = {12} 
-					style={styles.phone} keyboardType='phone-pad' />
-                </View>
-                <Text style={styles.header}>Доставка</Text>
-                <View style={styles.geoContainer}>
-                    <View style={styles.geoWrap}>
-                        <Text style={styles.inputWrapText}>Регион</Text>
-                        <TextInput value={region} style={styles.phone} onFocus={() => {setModal(true)}} />
-                    </View>
-                    <View style={styles.geoWrap}>
-                        <Text style={styles.inputWrapText}>Город, улица, дом</Text>
-                        <TextInput value={street} style={styles.phone} />
-                    </View>
-                </View>
-                <View style={styles.geoContainer}>
-                    <View style={styles.cellWrap}>
-                        <Text style={styles.inputWrapText}>Корпус</Text>
-                        <TextInput value={corpus} onChangeText={setCorpus} style={styles.phone} />
-                    </View>
-                    <View style={styles.cellWrap}>
-                        <Text style={styles.inputWrapText}>Квартира</Text>
-                        <TextInput value={apartament} onChangeText={setApartament} style={styles.phone} />
-                    </View>
-                    <View style={styles.cellWrap}>
-                        <Text style={styles.inputWrapText}>Этаж</Text>
-                        <TextInput value={stage} onChangeText={setStage} style={styles.phone} />
-                    </View>
-                    <View style={styles.cellWrap}>
-                        <Text style={styles.inputWrapText}>Домофон</Text>
-                        <TextInput value={doorphone} onChangeText={setDoorphone} style={styles.phone} />
-                    </View>
-                </View>
-                <View style={{alignItems: 'center', width: '100%'}}>
-                    <TouchableOpacity style={styles.geoButton} onPress={() => setMap(true)}>
-                        <View style={styles.buttonContainer}>
-                            <Text style={styles.geoText}>Выбрать на карте</Text>
-                            <Image source={path} resizeMode={'contain'} style={styles.geoImage} />
+                <View style={styles.blockConatiner}> 
+                    <Text style={styles.header}>Доставка</Text>
+                    <View style={styles.geoContainer}>
+                        <View style={[styles.geoWrap, {width: '40%'}]}>
+                            <Text style={styles.inputWrapText}>Город</Text>
+                            <TextInput value={region} style={styles.phone} onFocus={() => {setModal(true)}} />
                         </View>
-                    </TouchableOpacity>
+                        <View style={[styles.geoWrap, {width: '52%'}]}>
+                            <Text style={styles.inputWrapText}>Улица, дом</Text>
+                            <TextInput value={street} style={styles.phone} />
+                        </View>
+                    </View>
+                    <View style={styles.geoContainer}>
+                        <View style={styles.cellWrap}>
+                            <Text style={styles.inputWrapText}>Корпус</Text>
+                            <TextInput value={corpus} onChangeText={setCorpus} style={styles.phone} />
+                        </View>
+                        <View style={styles.cellWrap}>
+                            <Text style={styles.inputWrapText}>Квартира</Text>
+                            <TextInput value={apartament} onChangeText={setApartament} style={styles.phone} />
+                        </View>
+                        <View style={styles.cellWrap}>
+                            <Text style={styles.inputWrapText}>Этаж</Text>
+                            <TextInput value={stage} onChangeText={setStage} style={styles.phone} />
+                        </View>
+                        <View style={styles.cellWrap}>
+                            <Text style={styles.inputWrapText}>Домофон</Text>
+                            <TextInput value={doorphone} onChangeText={setDoorphone} style={styles.phone} />
+                        </View>
+                    </View>
+                    <View style={styles.geoContainer}>
+                        <View style={[styles.geoWrap, {width: '100%'}]}>
+                            <Text style={styles.inputWrapText}>Комментарий</Text>
+                            <TextInput value={comment} onChangeText={setComment} style={styles.phone} />
+                        </View>
+                    </View>
+                    <View style={{alignItems: 'center', width: '100%'}}>
+                        <TouchableOpacity style={styles.geoButton} onPress={() => setMap(true)}>
+                            <View style={styles.buttonContainer}>
+                                <Text style={styles.geoText}>Выбрать на карте</Text>
+                                <Image source={path} resizeMode={'contain'} style={styles.geoImage} />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <Text style={styles.header}>Оплата</Text>
-                <TouchableOpacity style={payType === "cash" ? styles.selectedContainer : styles.paymentContainer} 
-                onPress={() => setPayType("cash")}>
-                    <Text style={styles.paymentHeader}>Наличными</Text>
-                    <Text style={styles.paymentText}>Приготовьте нужную сумму</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={payType === "transfer" ? styles.selectedContainer : styles.paymentContainer} 
-                onPress={() => setPayType("transfer")}>
-                    <Text style={styles.paymentHeader}>Картой</Text>
-                    <Text style={styles.paymentText}>Приготовьте нужную сумму</Text>
-                </TouchableOpacity>
-                <View style={{alignItems: 'center', width: '100%', marginTop: 30}}>
+                <View style={styles.blockConatiner}>
+                    <Text style={styles.header}>Оплата</Text>
+                    <View style={{alignItems: 'center', width: '100%', marginTop: 30}}>
+                        <View>
+                            <Text>Способы оплаты</Text>
+                            <TouchableOpacity>
+                                <Text>Изменить</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Image />
+                            <Text></Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={[styles.blockConatiner, {borderBottomWidth: 0}]}>
+                    <Text style={[styles.header, {marginBottom: 12}]}>Данные получателя</Text>
+                    <View style={styles.inputWrap}>
+                        <Text style={styles.inputWrapText}>Имя</Text>
+                        <TextInput value={login} style={styles.phone} onChangeText={(text) => setLogin(text)} />
+                    </View>
+                    <View style={[styles.inputWrap, {width: '60%'}]}>
+                        <Text style={styles.inputWrapText}>Номер телефона</Text>
+                        <TextInput value={'+7' + phone} onChangeText={(text) => setPhone(text.slice(2))} maxLength = {12} 
+                        style={styles.phone} keyboardType='phone-pad' />
+                    </View>
                     <TouchableOpacity style={[styles.geoButton, {paddingVertical: 15}]} onPress={makeOrder}>
                         <View style={styles.buttonContainer}>
                             <Text style={styles.geoText}>Оформить заказ</Text>
@@ -168,55 +181,41 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff'
     },
     barContainer: {
-        width: '100%',
-        backgroundColor: 'black',
+        backgroundColor: '#fff',
         padding: 5,
+        paddingLeft: 25,
         paddingTop: Constants.statusBarHeight,
-        alignItems: 'center'
+        flexDirection: 'row',
+        borderBottomColor: '#f2f3f5',
+        borderBottomWidth: 3
+    },
+    barCell: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 30
     },
     barText: {
-        color: "#f1c40f",
-        fontWeight: "bold",
-        fontSize: 16,
-        marginVertical: 20
-    },
-    warning: {
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-        paddingTop: 20,
-        paddingBottom: 10,
-        backgroundColor: 'white',
-        width: '100%'
-    },
-    warningImage: {
-        width: 75,
-        height: 75,
-        marginRight: 15
-    },
-    warningText: {
-        fontFamily: 'Tahoma-Regular',
-        color: '#c0392b',
-        fontSize: 16,
-        marginBottom: 5
-    },
-    warningFull: {
-        width: '30%',
-        fontSize: 12,
-        fontFamily: 'Tahoma-Regular',
-    },
-    header: {
         fontWeight: "bold",
         fontSize: 20,
+        marginVertical: 20
+    },
+    blockConatiner: {
+        borderBottomColor: '#f2f3f5',
+        borderBottomWidth: 3,
+        paddingBottom: 20,
+        paddingHorizontal: 25
+    },
+    header: {
+        fontSize: 20,
         fontFamily: 'Tahoma-Regular',
-        padding: 15,
         paddingLeft: 10,
-        backgroundColor: '#f4f4f6',
-        marginTop: 10
+        paddingTop: 22,
+        paddingBottom: 8
     },
     inputWrap: {
 		padding: 5,
 		backgroundColor: "#f2f3f5",
-        marginTop: 10,
+        marginBottom: 20,
         marginHorizontal: 10,
 		borderRadius: 7,
 		width: '45%',
@@ -232,70 +231,43 @@ const styles = StyleSheet.create({
     },
     geoContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-around'
+        justifyContent: 'space-between'
     },
     geoWrap: {
         padding: 5,
 		backgroundColor: "#f2f3f5",
         marginTop: 10,
-		borderRadius: 7,
-		width: '45%',
+        borderRadius: 7,
+        marginBottom: 20
     },
     cellWrap: {
         padding: 5,
 		backgroundColor: "#f2f3f5",
-        marginVertical: 10,
+        marginBottom: 10,
 		borderRadius: 7,
 		width: '20%',
     },
     geoButton: {
-		backgroundColor: '#f1c40f',
-		paddingVertical: 10,
+		backgroundColor: '#f08741',
+		paddingVertical: 20,
 		marginHorizontal: 5,
-		borderRadius: 7,
-		width: '95%',
+		borderRadius: 15,
+		width: '100%',
 		alignItems: 'center'
 	},
 	geoText: {
 		fontFamily: 'Tahoma-Regular', 
-		fontSize: 18, 
+        fontSize: 16,
+        fontWeight: 'bold', 
 		color: 'white'
     },
     geoImage: {
-        width: 30,
-        height: 30,
+        width: 20,
+        height: 20,
         marginLeft: 20
     },
     buttonContainer: {
         flexDirection: 'row',
         alignItems: 'center'
-    },
-    paymentContainer: {
-        borderColor: "#efefef",
-        borderRadius: 8,
-        borderWidth: 1,
-        padding: 10,
-        paddingLeft: 20,
-        margin: 10,
-        marginBottom: 0
-    },
-    selectedContainer: {
-        borderColor: "#f1c40f",
-        borderRadius: 8,
-        borderWidth: 1,
-        padding: 10,
-        paddingLeft: 20,
-        margin: 10,
-        marginBottom: 0
-    },
-    paymentHeader: {
-        fontFamily: 'Tahoma-Regular', 
-        fontSize: 18,
-        marginBottom: 5 
-    },
-    paymentText: {
-        fontFamily: 'Tahoma-Regular', 
-		fontSize: 14, 
-        color: '#8b95a1'
     }
 });
